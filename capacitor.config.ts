87519b270fdc6c52ac2b3215b0c51ce605305a98
princekumarjha83-oss <princekumarjha83@gmail.com>
2026-04-19 21:33:55 +0530

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'react.example',
+  appName: 'react-example',
+  webDir: 'dist'
+};
+
+export default config;
